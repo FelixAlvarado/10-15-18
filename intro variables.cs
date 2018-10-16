a80de81c9748d41782acfa15457d40d0ebe7fe74
FelixAlvarado <felixalvarado653@gmail.com>
2018-10-15 17:27:03 -0700

    took intro variables notes

---
+++ intro variables.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tester : MonoBehaviour {
+
+	//f at the end means its a float. not having float will yeild an error
+
+	/*another comment */
+
+	//in c# must type type of variable, then name, then value
+
+	//want to give meaninful names 
+
+	//float of 32 bit number, and double is 64 bit number. doubles are more precise. double doesn't need ending  letter 
+	float power = 2.1f;
+
+	//could put d at end if you want to but not neccisary 
+	double health = 3.5;
+
+	//integers 
+
+	int score = 44;
+
+	//strings 
+
+	string name = "wizard";
+
+	//boolean 
+
+	bool isAlive = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
