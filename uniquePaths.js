7b263dcec2d9f413aa7c18ab60f2682cd60b5843
FelixAlvarado <felixalvarado653@gmail.com>
2018-10-17 13:25:30 -0700

    did unique path with obstacles solution

--- uniquePaths.js
+++ uniquePaths.js
@@ -29,3 +29,19 @@ var uniquePathsWithObstacles = function(obstacleGrid, cache = {}, x = 0, y = 0) 
     
     
 };
+
+
+//fast and very accurate solution for solving uniquePathWithObstacles!!!!!
+var uniquePathsWithObstacles = function(obstacleGrid, cache = {}, x = 0, y = 0) {
+    let width = obstacleGrid[0].length;
+    let dp = new Array(width).fill(0);
+    dp[0] = 1;
+    for(let i = 0; i < obstacleGrid.length; i++){
+        for(let j = 0; j < width; j++){
+            if (obstacleGrid[i][j] === 1){
+                dp[j] = 0;
+            }else if (j > 0) dp[j] += dp[j - 1];
+        }
+    }
+    return dp[width - 1];
+};
