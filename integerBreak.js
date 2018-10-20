1d4be0c547544b94aa48e771359bfc6df0310233
FelixAlvarado <felixalvarado653@gmail.com>
2018-10-20 14:19:53 -0700

    finished integerBreak solution

--- integerBreak.js
+++ integerBreak.js
@@ -13,3 +13,15 @@ var integerBreak = function(n, cache = {6:9,5:6,4:4,3:2,2:1}) {
     }
     return cache[n];
 };
+
+function integerBreak2(n) {
+    if(n === 2) return 1;
+    if(n === 3) return 2;
+    let product = 1;
+    while (n > 4) {
+        product *= 3;
+        n -= 3;
+    }
+    product *= n;
+    return product;
+}
